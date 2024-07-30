25ce770a9d5583a5df8c41a4bcdb8401d2b4db1d
Parth Agrawal <pvagrawal0@gmail.com>
2024-07-30 17:33:25 -0400

    userqueue is able to clean every ten seconds

--- cmd/backend/clickHandler.go
+++ cmd/backend/clickHandler.go
@@ -3,9 +3,52 @@ package backend
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/redis/go-redis/v9"
 )
+
+var store *redis.Client
+
+
+func init () { 
+	store = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+	go startQueueCleaner()
+}
+
+func startQueueCleaner() {
+
+	fmt.Println("starting cleaner")
+
+	ticker := time.NewTicker(10 * time.Second)
+	ctx := context.Background()
+
+	for range ticker.C { 
+		
+		fmt.Println("starting cleaning, iterating users")
+		users, err := store.Keys (ctx, "*").Result()
+		if err != nil {
+			fmt.Printf("error getting user key: %v", err)
+			continue
+		}
+
+		fmt.Println("starting to remove items")
+		
+		for _, user := range users { 
+			_, err := store.RPop(ctx, user).Result()
+			if err != nil && err != redis.Nil { 
+				fmt.Printf("error removing item %v", err)
+			}
+		}
+	}
+
+
+}
+
 
 func ClickHandler(user string, timestamp int) error {
 	// the click handler should essentially take in the user
@@ -16,11 +59,7 @@ func ClickHandler(user string, timestamp int) error {
 
 	// if it was not sucessfully added it should immediately return an error which should be returned
 	
-	store := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "",
-		DB:       0,
-	})
+
 
 
 	var ctx = context.Background()
@@ -36,7 +75,7 @@ func ClickHandler(user string, timestamp int) error {
 		return fmt.Errorf("failed to create fruits queue: %w", err)
 	}
 
-	fmt.Println("we're here in the clickHandler. the user is %s and the timestamp is %s", user, timestamp)
+	fmt.Println("we're here in the clickHandler. user and timestamp are ", user, timestamp)
 
 	// read queue size
 	queueLength, err := store.LLen(ctx, user).Result()
@@ -55,6 +94,9 @@ func ClickHandler(user string, timestamp int) error {
 	if err != nil {
 		return fmt.Errorf("failed to add timestamp to queue: %w", err)
 	}
+
+
+
 
 
 	return err
